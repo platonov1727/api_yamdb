c5a3aeb5638a9924b15ea873e87e71dc6513a7d3
Сергей Платонов <platonov1727@yandex.ru>
2022-12-18 11:37:10 +0300

    Fixed Migrations

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -92,34 +92,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
-
-
-class Review(models.Model):
-    title = models.ForeignKey(Title,
-                              on_delete=models.CASCADE,
-                              related_name='reviews')
-    text = models.TextField(max_length=4000)
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='rewiews')
-    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
-    score = models.IntegerField(
-        validators=[MinValueValidator(1),
-                    MaxValueValidator(10)])
-
-
-class Comment(models.Model):
-    author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name='comments')
-    text = models.TextField(max_length=500)
-    pub_date = models.DateTimeField(auto_now_add=True, db_index=True)
-    review = models.ForeignKey(Review,
-                               on_delete=models.CASCADE,
-                               related_name='comments')
-    class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
-
-    def __str__(self):
-        return self.text
