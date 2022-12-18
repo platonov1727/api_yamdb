2764df04b570f5d2206881d6f51dc57a7bafdfca
Сергей Платонов <platonov1727@yandex.ru>
2022-12-18 14:33:17 +0300

    Merged

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -54,12 +54,3 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
-
-
-class ReviewSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(read_only=True,
-                                          slug_field='username')
-
-    class Meta:
-        fields = '__all__'
-        model = Review
