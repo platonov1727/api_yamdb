5c07b29b57b2c85c7bbb631ba30e147fc856e2b6
Дмитрий Шабанов <shabanov010@yandex.ru>
2022-12-20 17:36:08 +0300

    dont warry

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -34,3 +34,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username', )
