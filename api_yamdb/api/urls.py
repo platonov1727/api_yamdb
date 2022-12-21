625e90fc1ecda75615a6d1f05f82317d41c71b5e
Maria Rozhina <mkrozhina@gmail.com>
2022-12-22 00:07:41 +0900

    put everything in users app

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (AdminAPI, CategoryViewSet, CommentViewSet, GenreViewSet,
-                    RegistrationAPI, ReviewViewSet, TitleViewSet, TokenAPI,
-                    UserMePatchView)
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet)
 
 router = DefaultRouter()
 
-router.register('users', AdminAPI)
 router.register(r'titles', TitleViewSet)
 router.register(r'genres', GenreViewSet)
 router.register(r'categories', CategoryViewSet)
@@ -20,8 +18,5 @@ router.register(
     basename='coomments')
 
 urlpatterns = [
-    path('v1/users/me/', UserMePatchView.as_view()),
     path('v1/', include(router.urls)),
-    path('v1/auth/signup/', RegistrationAPI.as_view()),
-    path('v1/auth/token/', TokenAPI.as_view()),
 ]
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,29 +1,16 @@
-from django.contrib.auth.tokens import default_token_generator
-from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, permissions, status, viewsets
+from rest_framework import filters, viewsets
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 from titles.models import Category, Genre, Title
-from reviews.models import Review, Comment
-from users.models import User
+from reviews.models import Review
 from rest_framework import mixins
 
-from rest_framework.decorators import action, api_view, permission_classes
-
-from api_yamdb.settings import DEFAULT_FROM_EMAIL
-
 from .filters import TitlesFilter
-from .permissions import IsAdmin, IsAdminOrReadOnly, IsAdminModeratorOwnerOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsAdminModeratorOwnerOrReadOnly
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer, TitleSerializer,
-                          TokenSerializer, UserSerializer,
-                          RegisterDataSerializer, AdminSerializer,
                           ReadOnlyTitleSerializer)
 
 
@@ -64,39 +51,6 @@ class CategoryViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
     lookup_field = 'slug'
 
 
-class AdminAPI(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = AdminSerializer
-    lookup_field = 'username'
-    permission_classes = (IsAdmin, )
-    filter_backends = (filters.SearchFilter, )
-    search_fields = ('username', )
-    http_method_names = ['get', 'post', 'list', 'patch', 'delete']
-
-
-class UserMePatchView(APIView):
-
-    def get(self, request):
-        if request.user.is_authenticated:
-            user = get_object_or_404(User, id=request.user.id)
-            serializer = UserSerializer(user)
-            return Response(serializer.data)
-        return Response('Вы не авторизованы',
-                        status=status.HTTP_401_UNAUTHORIZED)
-
-    def patch(self, request):
-        if request.user.is_authenticated:
-            user = get_object_or_404(User, id=request.user.id)
-            serializer = UserSerializer(user, data=request.data, partial=True)
-            if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
-        return Response('Вы не авторизованы',
-                        status=status.HTTP_401_UNAUTHORIZED)
-
-
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsAdminModeratorOwnerOrReadOnly]
@@ -125,48 +79,3 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title_id = self.kwargs.get('title_id')
         title = get_object_or_404(Title, id=title_id)
         serializer.save(author=self.request.user, title=title)
-
-
-class RegistrationAPI(APIView):
-    """Аутентификация через верификацию email"""
-    permission_classes = (AllowAny, )
-
-    def post(self, request):
-        serializer = RegisterDataSerializer(data=request.data)
-        if serializer.is_valid():
-            if User.objects.filter(
-                    username=serializer.validated_data['username'],
-                    email=serializer.validated_data['email']).exists():
-                user = get_object_or_404(
-                    User,
-                    username=serializer.validated_data['username'],
-                    email=serializer.validated_data['email'])
-            else:
-                serializer.save()
-                user = get_object_or_404(
-                    User, username=serializer.validated_data['username'])
-            confirmation_code = default_token_generator.make_token(user)
-            send_mail('Подтверждение регистрации',
-                      f'Подтвердите ваш e-mail: {confirmation_code}',
-                      DEFAULT_FROM_EMAIL, [serializer.data['email']],
-                      fail_silently=False)
-            return Response(serializer.data, status.HTTP_200_OK)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
-
-class TokenAPI(APIView):
-
-    permission_classes = (AllowAny, )
-    serializer_class = TokenSerializer
-
-    def post(self, request):
-        serializer = TokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = get_object_or_404(User, username=serializer.data['username'])
-
-        if default_token_generator.check_token(
-                user, serializer.validated_data["confirmation_code"]):
-            token = AccessToken.for_user(user)
-            return Response({"token": str(token)}, status=status.HTTP_200_OK)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
